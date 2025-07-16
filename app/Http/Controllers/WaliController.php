56f12d7e51e01c9d622586f84e5bc57a4f163d61
Prita Puja Astuti <prita.pujaastuti@gmail.com>
2025-07-16 13:58:50 +0700

    feat: initial commit

---
+++ app/Http/Controllers/WaliController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Models\User as Model;
+
+class WaliController extends Controller
+{
+    private $viewIndex = 'user_index';
+    private $viewCreate = 'user_form';
+    private $viewEdit = 'user_form';
+    private $viewShow = 'user_show';
+    private $routePrefix = 'wali';
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('operator.' . $this->viewIndex, [
+            'models' => Model::where('akses', 'wali')
+                ->latest()
+                ->paginate(50),
+            'routePrefix' => $this->routePrefix,
+            'title' => 'Data Wali Murid',
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $data = [
+            'model' => new Model(),
+            'method' => 'POST',
+            'route' => $this->routePrefix . '.store',
+            'button' => 'SIMPAN',
+            'title' => 'Form Data Wali Murid'
+        ];
+        return view('operator.' . $this->viewCreate, $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $requestData = $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'nohp' => 'required|unique:users',
+            'password' => 'required'
+
+        ]);
+        $requestData['password'] = bcrypt($requestData['password']);
+        $requestData['email_verified_at'] = now();
+        $requestData['akses'] = 'wali';
+        Model::create($requestData);
+        flash('Data berhasil disimpan');
+        return redirect()->route($this->routePrefix . '.index');
+        // return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data = [
+            'model' => Model::findOrFail($id),
+            'method' => 'PUT',
+            'route' => [$this->routePrefix . '.update', $id],
+            'button' => 'UPDATE',
+            'title' => 'Form Data Wali Murid'
+        ];
+        return view('operator.' . $this->viewEdit, $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $requestData = $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email,' . $id,
+            'nohp' => 'required|unique:users,nohp,' . $id,
+            'password' => 'nullable'
+
+        ]);
+        $model = Model::findOrFail($id);
+        if ($requestData['password'] == "") {
+            unset($requestData['password']);
+        } else {
+            $requestData['password'] = bcrypt($requestData['password']);
+        }
+        $model->fill($requestData);
+        $model->save();
+        flash('Data berhasil diubah');
+        return redirect()->route($this->routePrefix . '.index');
+        // return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $model = Model::where('akses', 'wali')->firstOrFail();
+
+        $model->delete();
+        flash('Data berhasil dihapus');
+        return back();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Controllers\BerandaOperatorController;
+use App\Http\Controllers\BerandaWaliController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WaliController;
+use App\Http\Middleware\Operator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('operator')->middleware(['auth', 'auth.operator'])->group(function () {
+    // ini route khusus untuk operator
+    Route::get('beranda', [BerandaOperatorController::class, 'index'])->name('operator.beranda');
+    Route::resource('user', UserController::class);
+    Route::resource('wali', WaliController::class);
+
+});
+
+Route::prefix('wali')->middleware(['auth', 'auth.wali'])->group(function () {
+    // ini route khusus untuk wali
+    Route::get('beranda', [BerandaWaliController::class, 'index'])->name('wali.beranda');
+});
+
+Route::get('logout', function(){
+    Auth::logout();
+    return redirect('login');
+})->name('logout');
