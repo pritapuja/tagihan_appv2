2cac2c3bd5e49ddafd63991f1149e4b38b213d64
rora <pujaprita7@gmail.com>
2025-07-17 18:32:25 +0700

    feat(siswa): add controller, model, and route

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Siswa;
-use App\Http\Requests\StoreSiswaRequest;
-use App\Http\Requests\UpdateSiswaRequest;
+use Illuminate\Http\Request;
+use \App\Models\Siswa as Model;
 
 class SiswaController extends Controller
 {
+    private $viewIndex = 'siswa_index';
+    private $viewCreate = 'siswa_form';
+    private $viewEdit = 'siswa_form';
+    private $viewShow = 'siswa_show';
+    private $routePrefix = 'wali';
+
+
     /**
      * Display a listing of the resource.
      *
@@ -15,72 +21,13 @@ class SiswaController extends Controller
      */
     public function index()
     {
-        //
+        return view('operator.' . $this->viewIndex, [
+            'models' => Model::latest()
+                ->paginate(50),
+            'routePrefix' => $this->routePrefix,
+            'title' => 'Data Siswa',
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreSiswaRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreSiswaRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Siswa  $siswa
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Siswa $siswa)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Siswa  $siswa
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Siswa $siswa)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateSiswaRequest  $request
-     * @param  \App\Models\Siswa  $siswa
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateSiswaRequest $request, Siswa $siswa)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Siswa  $siswa
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Siswa $siswa)
-    {
-        //
-    }
+    
 }
--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Siswa extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the user that owns the Siswa
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function wali(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'wali_id');
+    }
 }
